7603375030b0fe3340a9ff1b12ec1f85dd366978
Daniel Manuel <dmanueljr@gmail.com>
2015-05-15 09:44:00 -0700

    controller saves images to firebase

--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -2,28 +2,39 @@ angular
 	.module('tictactoeApp') // <-- DO NOT EVER PUT A SEMI COLON HERE!!!
 	.controller('TictactoeController', TictactoeController);
 				//'UPPERCASE', UPPERCASE
-	TictactoeController.$inject = ['$firebaseObject'];
+	TictactoeController.$inject = ['$firebaseArray'];
 
-	function TictactoeController($firebaseObject) {
+	function TictactoeController($firebaseArray) {
 		var self = this;
 		self.game = (function() {
             var ref = new Firebase("https://tictactoe-app.firebaseio.com/");
-            var game = $firebaseObject(ref);
+            var game = $firebaseArray(ref);
             return game;            
         })();
 
+
 	// initial setup of tic-tac-toe game board
 		// self.boxes = [];
-		self.getBoxes = getBoxes();
+		self.getBoxes = getBoxes;
+		self.playerMove = playerMove;
 
 		function getBoxes() {
 			for (var i = 0; i < 9; i++) {			
-				self.game.$add({whoIsHere: "ghost"});
+				self.game.$add({whoIsHere: "empty"});
 			}
 		}
 
 	// determines player move and alternate player turn
+		function playerMove ($index) {
+			console.log(self.game[$index].whoIsHere)
+			self.game[$index].whoIsHere = "pacman";
+			self.game.$save(self.game[$index]);
 
+	// ASK!!	==> // console.log(self.game.whoIsHere[$index]);
+			
+
+			// .whoIsHere[$index] = "pacman";
+		}
 
 
 	// gets winning combination 
