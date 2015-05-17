318b8d178013d5b40b9065fa83e4f93bcb47d85e
Daniel Manuel <dmanueljr@gmail.com>
2015-05-16 18:51:46 -0700

    saves working state for backup

--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -2,39 +2,76 @@ angular
 	.module('tictactoeApp') // <-- DO NOT EVER PUT A SEMI COLON HERE!!!
 	.controller('TictactoeController', TictactoeController);
 				//'UPPERCASE', UPPERCASE
-	TictactoeController.$inject = ['$firebaseArray'];
+	TictactoeController.$inject = ['$firebaseObject'];
 
-	function TictactoeController($firebaseArray) {
+	function TictactoeController($firebaseObject) {
+
 		var self = this;
+		self.playerMove = playerMove;
+		
+		// firebase connection to setup game board
 		self.game = (function() {
-            var ref = new Firebase("https://tictactoe-app.firebaseio.com/");
-            var game = $firebaseArray(ref);
+            var ref = new Firebase("https://my-tictactoe-app.firebaseio.com/game-board");
+            var game = $firebaseObject(ref);
             return game;            
         })();
 
+        // fire base connecdtion for players
+        self.play = (function() {
+        	var ref = new Firebase("https://my-tictactoe-app.firebaseio.com/");
+        	var play = $firebaseObject(ref);
+        	return play
+        })();
 
-	// initial setup of tic-tac-toe game board
-		// self.boxes = [];
-		self.getBoxes = getBoxes;
-		self.playerMove = playerMove;
-
-		function getBoxes() {
-			for (var i = 0; i < 9; i++) {			
-				self.game.$add({whoIsHere: "empty"});
+		// initial setup of tic-tac-toe game board
+		self.getBoxes = (function() {
+			for (var i = 0; i < 9; i++) {
+				self.game[i] = {whoIsHere: "null"}	
+				self.game.$save();
 			}
+			console.log(self.game);
+		})();
+
+		// determines player move and alternate player turn
+		function playerMove ($index) {
+
+			// alternate turns
+			if (self.play.moveCounter % 2 === 0) {
+				self.game[$index].whoIsHere = "pacman";
+			}	else {
+				self.game[$index].whoIsHere = "ghost";
+			}
+
+			console.log(self.play.winningCombo[0])
+			console.log(self.play.icons)
+
+
+			// resets counter
+			if (self.play.moveCounter < 9) {
+				self.play.moveCounter += 1;	
+			}	else {
+				self.play.moveCounter = 0;
+			}
+
+			// saves and checks logs
+			self.play.$save();
+			self.game.$save();
+			console.log(self.play.moveCounter);
+			console.log(self.game[$index].whoIsHere);
+			console.log($index);
 		}
 
-	// determines player move and alternate player turn
-		function playerMove ($index) {
-			console.log(self.game[$index].whoIsHere)
-			self.game[$index].whoIsHere = "pacman";
-			self.game.$save(self.game[$index]);
+
+		// determines winning move
+
+		
+
+
+
 
 	// ASK!!	==> // console.log(self.game.whoIsHere[$index]);
-			
 
-			// .whoIsHere[$index] = "pacman";
-		}
+	// .whoIsHere[$index] = "pacman";
 
 
 	// gets winning combination 
